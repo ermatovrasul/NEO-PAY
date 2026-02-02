28f7e5b064c636f83e3467c0f9b128e70a301c0c
Расул Ерматов <rasulermatov015@gmail.com>
2026-02-03 03:08:32 +0600

    video fix

--- components/VideoInstruction.tsx
+++ components/VideoInstruction.tsx
@@ -48,10 +48,11 @@ export const VideoInstruction = () => {
             ) : (
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/zaiyCoWyjkg?autoplay=1"
+                src="https://www.youtube.com/embed/zaiyCoWyjkg"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
               ></iframe>
             )}
